980f5e58aa14f71fcd4ee443b5977b661d6e2669
CodeWithRibo <riboluna2818@gmail.com>
2024-11-01 19:16:26 -0700

    pushing all commited

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      roman: ["Times New Roman"],
+      nunito: ["Nunito"],
+    },
+    extend: {
+      colors: {
+        primary: "#b91c1c", // Define your primary color here
+        secondary: "#e09241", 
+      },
+    },
+  },
+  plugins: [],
+};
